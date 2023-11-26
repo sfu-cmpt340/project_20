177caed4b8fc068fcfb451a770f51dd5ddc82051
wca78Cao <106790220+wca78Cao@users.noreply.github.com>
2023-11-26 12:02:10 -0800

    Update LungAnalyze.m

--- LungAnalyze.m
+++ LungAnalyze.m
@@ -1,22 +1,53 @@
 % Image Processing Toolbox Add-On
-lungs_img = imread("test_lungs.jpg");
 
-grayImg = rgb2gray(lungs_img);
+%% 
+% Specify the folder where the files live.
+myFolder = 'C:\Users\yourUserName\Documents\My Pictures';
+% Check to make sure that folder actually exists.  Warn user if it doesn't.
+if ~isfolder(myFolder)
+    errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', myFolder);
+    uiwait(warndlg(errorMessage));
+    myFolder = uigetdir(); % Ask for a new one.
+    if myFolder == 0
+         % User clicked Cancel
+         return;
+    end
+end
+% Get a list of all files in the folder with the desired file name pattern.
+filePattern = fullfile(myFolder, '*.png'); % Change to whatever pattern you need.
+theFiles = dir(filePattern);
+for k = 1 : length(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
+    % Now do whatever you want with this file name,
+    % such as reading it in as an image array with imread()
+    % imageArray = imread(fullFileName);
+    % imshow(imageArray);  % Display image.
+    %drawnow; % Force display to update immediately.
+    lungs_img = imread(fullFileName);
+    edge_detections(lungs_img);
+end
 
-detection_types = ["Sobel", "Prewitt", "Roberts", "Log", "Canny", "Canny_old", "Zerocross", "Approxcanny"];
+%% 
+function edgeDe = edge_detections(lungs_img)
+    grayImg = rgb2gray(lungs_img);
 
-figure;
-subplot(3, 3, 1);
-imshow(grayImg);
-title("Original Image");
+    detection_types = ["Sobel", "Prewitt", "Roberts", "Log", "Canny", "Canny_old", "Zerocross", "Approxcanny"];
 
-plot_number = 2;
-for type = detection_types
-    edges = edge(grayImg, type);
-    subplot(3, 3, plot_number);
-    plot_number = plot_number + 1;
-    imshow(edges);
-    title(type);
+    figure;
+    subplot(3, 3, 1);
+    imshow(grayImg);
+    title("Original Image");
+
+    plot_number = 2;
+    for type = detection_types
+        edges = edge(grayImg, type);
+        subplot(3, 3, plot_number);
+        plot_number = plot_number + 1;
+        imshow(edges);
+        title(type);
+    end
 end
 
 
