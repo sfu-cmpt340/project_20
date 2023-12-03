0b12dfc5903b845480766dbe4c78d08541542ad6
Dalveer Dosanjh <dsd6@sfu.ca>
2023-12-02 16:45:33 -0800

    cleaned up some code

--- LungAnalyze.m
+++ LungAnalyze.m
@@ -1,73 +1,7 @@
-% % Lung CT Image Analyzer App
-% 
-% disp('Welcome to the Lung CT Image Analyzer App!');
-% disp('This app allows you to view and analyze lung CT images.');
-% disp('You can choose a lung CT image (1-20) for analysis, and');
-% disp('the app will display the original image and perform edge detection.');
-% disp('You can choose to continue analyzing more images or exit the app.');
-% disp("--------------------------------------------------------------------------------");
-% 
-% while true
-%     % Ask the user for the lung CT image number
-%     image_number = input('Enter the lung CT image number to view (1-20), or enter 0 to exit: ');
-% 
-%     % Check if the user wants to exit
-%     if image_number == 0
-%         disp('Exiting program.');
-%         break;  % Exit the while loop
-%     end
-% 
-%     % Validate the input
-%     if image_number < 1 || image_number > 20
-%         disp('Invalid input. Please enter a number between 1 and 20.');
-%         continue;  % Skip the rest of the loop and ask for input again
-%     end
-% 
-%     % Construct the image filename
-%     image_filename = sprintf('test_lungs_%02d.jpg', image_number);
-% 
-%     % Try to load the image
-%     try
-%         % Image Processing Toolbox Add-On
-%         lungs_img = imread(image_filename);
-% 
-%         grayImg = rgb2gray(lungs_img);
-% 
-%         detection_types = ["Sobel", "Prewitt", "Roberts", "Log", "Canny", "Canny_old", "Zerocross", "Approxcanny"];
-% 
-%         figure;
-%         subplot(3, 3, 1);
-%         imshow(grayImg);
-%         title("Original Image");
-% 
-%         plot_number = 2;
-%         for type = detection_types
-%             edges = edge(grayImg, type);
-%             subplot(3, 3, plot_number);
-%             plot_number = plot_number + 1;
-%             imshow(edges);
-%             title(type);
-%         end
-%     catch
-%         % Catch any errors during image loading or processing
-%         disp('Error loading or processing the image. Please choose another image.');
-%         continue;  % Skip the rest of the loop and ask for input again
-%     end
-% 
-%     % Ask the user if they want to continue
-%     continue_choice = input('Do you want to generate another lung image? (yes/no): ', 's');
-% 
-%     if ~strcmpi(continue_choice, 'yes')
-%         disp('Exiting program.');
-%         break;  % Exit the while loop
-%     end
-% end
-% 
-% % Image Processing Toolbox Add-On
+%% Find folder files and check images
+% Specify the folder where the files live [samples,samples-normal].
+myFolder = 'samples';
 
-%% 
-% Specify the folder where the files live.
-myFolder = 'samples-normal';
 % Check to make sure that folder actually exists.  Warn user if it doesn't.
 if ~isfolder(myFolder)
     errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', myFolder);
@@ -99,13 +33,12 @@ for k = 1 : length(theFiles)
     
     lungs_img = imread(fullFileName);
     num_of_circles = check_circles(lungs_img, [0.22 0.6], 0.84, [0.21 0.55], 0.88);
-    imgPercent = round(num_of_circles/ (13+(k/10)), 4);
+    imgPercent = round(num_of_circles/ (25+(k/10)), 4);
 
     circlePercentage(k) = imgPercent;
     itemNames(k) = {[baseFileName ' ' num2str(imgPercent*100) '% likely to have mass']};
     fileNames(k) = {fullFileName};
 end
-
 
 [~, sortedIndices] = sort(circlePercentage, 'descend');
 
@@ -114,7 +47,7 @@ fileNames = fileNames(sortedIndices);
 circlePercentage = circlePercentage(sortedIndices);
 
 
-%%
+%% Creating the GUI
 % Create a figure
 fig = figure('Name', 'GUI', 'units', 'normalized', 'Position', [0.2, 0.2, 0.6, 0.6]);
 
@@ -139,7 +72,7 @@ function listboxCallback(src, event)
 end
 
 
-%%
+%% Function to show circles GUI
 function [num_of_circles] = find_circles(img, img_name, intensity_bw, circle_sens_bw, intensity_edge, circle_sens_edge)
     % find_circles  tries to detect circles in the img based on the provided
     % thresholds
@@ -168,12 +101,10 @@ function [num_of_circles] = find_circles(img, img_name, intensity_bw, circle_sen
 
     img = im2gray(img);
 
-    % detect circles
-    % method = 'bw';
+
     img_adjusted_bw = imadjust(img, intensity_bw);
     subplot(1,3,2);
-    %     imshow(img_adjusted)
-    %     title("Contrast Adjusted");
+
         
     img_BW = imbinarize(img_adjusted_bw);
     imshow(img_adjusted_bw)
@@ -190,7 +121,6 @@ function [num_of_circles] = find_circles(img, img_name, intensity_bw, circle_sen
         disp("No circles found.");
     end
     
-    % method = 'edge';
     img_adjusted_edge = imadjust(img, intensity_edge);
     subplot(1,3,3);
         
@@ -211,19 +141,8 @@ function [num_of_circles] = find_circles(img, img_name, intensity_bw, circle_sen
         disp("No circles found.");
     end
 end
-
+%% Function to check for circles
 function [num_of_circles] = check_circles(img, intensity_bw, circle_sens_bw, intensity_edge, circle_sens_edge)
-    % find_circles  tries to detect circles in the img based on the provided
-    % thresholds
-    %   find_circles(img, [min_in max_in], circle_sens) adjusts img contrast
-    %   based on [min_in max_in] and detects circles based on circle_sens
-    % Inputs:
-    %   img         : the image to detect circles in
-    %   intensity   : [min = 0...1 max = 0...1], min < max; a double vector representing the
-    %   thresholds to base contrast adjustment on
-    %   circle_sens : 0...1; the threshold for circle detection. Greater values
-    %   are less sensitive
-    %   method      : the filter to use circle detection on
     arguments
       img
       intensity_bw (1,2) double = [0.2 0.6]
